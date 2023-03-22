a748b8e28b284bf5041dd226ae0448dbf584b71d
averu <neuf-seize@outlook.jp>
2023-03-22 22:54:03 +0900

    アイコンの追加

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+  all(not(debug_assertions), target_os = "windows"),
+  windows_subsystem = "windows"
+)]
+
 use obws::{Client, Error, requests};
 use serde::{Deserialize, Serialize};
 use serde_json::{self, Map, Value};
